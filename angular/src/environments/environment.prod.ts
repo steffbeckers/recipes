38e8b48e2c299530f1ec1099a6bee3a0e58d11ba
Steff Beckers <steff@steffbeckers.eu>
2021-09-10 15:32:51 +0200

    Angular baseUrl /

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 import { Environment } from '@abp/ng.core';
 
-const baseUrl = 'http://localhost:4200';
+const baseUrl = "/";
 
 export const environment = {
   production: true,
@@ -10,7 +10,7 @@ export const environment = {
     logoUrl: '',
   },
   oAuthConfig: {
-    issuer: 'https://localhost:44320',
+    issuer: baseUrl,
     redirectUri: baseUrl,
     clientId: 'Recipes_App',
     responseType: 'code',
@@ -19,7 +19,7 @@ export const environment = {
   },
   apis: {
     default: {
-      url: 'https://localhost:44320',
+      url: baseUrl,
       rootNamespace: 'Recipes',
     },
   },
